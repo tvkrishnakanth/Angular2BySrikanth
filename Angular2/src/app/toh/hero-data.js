15ca15363521632e11cea3076f875ec52b4e9d66
Srikanth Machineni <smachineni@deloitte.com>
2017-06-21 18:31:30 +0530

    Added http Url for images

--- Angular2/src/app/toh/hero-data.js
+++ Angular2/src/app/toh/hero-data.js
@@ -1,15 +1,15 @@
 "use strict";
 exports.HeroData = [
-    { id: 1, name: 'Ravi', imageUrl: '../../images/img1.jpg' },
-    { id: 2, name: 'Roopesh', imageUrl: '../../images/img2.jpg' },
-    { id: 3, name: 'Krishna Kanth', imageUrl: '../../images/img3.jpg' },
-    { id: 4, name: 'Naresh', imageUrl: '../../images/img4.jpg' },
-    { id: 5, name: 'Srikanth', imageUrl: '../../images/img5.jpg' },
-    { id: 6, name: 'Sid', imageUrl: '../../images/img6.jpg' },
-    { id: 7, name: 'Vikram', imageUrl: '../../images/img7.jpg' },
-    { id: 8, name: 'Nikhil', imageUrl: '../../images/img8.jpg' },
-    { id: 9, name: 'Sashi', imageUrl: '../../images/img9.jpg' },
-    { id: 10, name: 'Naresh', imageUrl: '../../images/img10.jpg' }
+    { id: 1, name: 'Ravi', imageUrl: 'https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/Bxlk0Biiisqgwgje/videoblocks-handsome-employee-working-on-the-computer-in-the-modern-office-innovative-man-wearing-eyeglasses-and-jacket-managing-start-up-company_b1xg3p10e_thumbnail-small06.jpg' },
+    { id: 2, name: 'Roopesh', imageUrl: 'https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/Bxlk0Biiisqgwgje/videoblocks-handsome-employee-working-on-the-computer-in-the-modern-office-innovative-man-wearing-eyeglasses-and-jacket-managing-start-up-company_b1xg3p10e_thumbnail-small06.jpg' },
+    { id: 3, name: 'Krishna Kanth', imageUrl: 'https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/Bxlk0Biiisqgwgje/videoblocks-handsome-employee-working-on-the-computer-in-the-modern-office-innovative-man-wearing-eyeglasses-and-jacket-managing-start-up-company_b1xg3p10e_thumbnail-small06.jpg' },
+    { id: 4, name: 'Naresh', imageUrl: 'https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/Bxlk0Biiisqgwgje/videoblocks-handsome-employee-working-on-the-computer-in-the-modern-office-innovative-man-wearing-eyeglasses-and-jacket-managing-start-up-company_b1xg3p10e_thumbnail-small06.jpg' },
+    { id: 5, name: 'Srikanth', imageUrl: 'https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/Bxlk0Biiisqgwgje/videoblocks-handsome-employee-working-on-the-computer-in-the-modern-office-innovative-man-wearing-eyeglasses-and-jacket-managing-start-up-company_b1xg3p10e_thumbnail-small06.jpg' },
+    { id: 6, name: 'Sid', imageUrl: 'https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/Bxlk0Biiisqgwgje/videoblocks-handsome-employee-working-on-the-computer-in-the-modern-office-innovative-man-wearing-eyeglasses-and-jacket-managing-start-up-company_b1xg3p10e_thumbnail-small06.jpg' },
+    { id: 7, name: 'Vikram', imageUrl: 'https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/Bxlk0Biiisqgwgje/videoblocks-handsome-employee-working-on-the-computer-in-the-modern-office-innovative-man-wearing-eyeglasses-and-jacket-managing-start-up-company_b1xg3p10e_thumbnail-small06.jpg' },
+    { id: 8, name: 'Nikhil', imageUrl: 'https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/Bxlk0Biiisqgwgje/videoblocks-handsome-employee-working-on-the-computer-in-the-modern-office-innovative-man-wearing-eyeglasses-and-jacket-managing-start-up-company_b1xg3p10e_thumbnail-small06.jpg' },
+    { id: 9, name: 'Sashi', imageUrl: 'https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/Bxlk0Biiisqgwgje/videoblocks-handsome-employee-working-on-the-computer-in-the-modern-office-innovative-man-wearing-eyeglasses-and-jacket-managing-start-up-company_b1xg3p10e_thumbnail-small06.jpg' },
+    { id: 10, name: 'Naresh', imageUrl: 'https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/Bxlk0Biiisqgwgje/videoblocks-handsome-employee-working-on-the-computer-in-the-modern-office-innovative-man-wearing-eyeglasses-and-jacket-managing-start-up-company_b1xg3p10e_thumbnail-small06.jpg' }
 ];
 /*
 Copyright 2017 Google Inc. All Rights Reserved.
